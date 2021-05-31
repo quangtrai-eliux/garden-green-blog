c726bd1b20e6ce1cdeb33ed4a832842201c66e6d
Nguyen Quang Trai <quangtrai.zeli@gmail.com>
2021-05-31 15:27:23 +0700

    create migrations and define database in migration

---
+++ blog/database/migrations/2021_05_31_072732_create_posts_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->mediumText('title');
+            $table->mediumText('description');
+            $table->longText('content');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')
+            ->references('id')
+            ->on('users')
+            ->onDelete('cascade');
+            $table->unsignedBigInteger('post_type_id');
+            $table->foreign('post_type_id')
+            ->references('id')
+            ->on('post_type')
+            ->onDelete('cascade');
+            $table->integer('view');
+            $table->integer('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
---
+++ blog/database/migrations/2021_05_31_080434_create_advs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdvsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('advs', function (Blueprint $table) {
+            $table->id();
+            $table->integer('number_block');
+            $table->text('uri');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('advs');
+    }
+}
