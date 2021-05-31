ac290c2e84f3e3a8ee74893d675d3d0294549eae
Nguyen Quang Trai <quangtrai.zeli@gmail.com>
2021-05-31 17:31:05 +0700

    create model, factory, seeder and controller have resource according migrations

---
+++ blog/database/factories/AdvFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Adv;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AdvFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Adv::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
