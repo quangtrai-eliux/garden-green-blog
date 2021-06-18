006b3e024a35f9337152bb1bbccd3576db49ad09
Nguyen Quang Trai <quangtrai.zeli@gmail.com>
2021-06-18 21:20:46 +0700

    create routes for admin

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,6 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdvController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\PostTagController;
+use App\Http\Controllers\PostTypeController;
+use App\Http\Controllers\TagController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +23,22 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/admin', function () {
+    return view('dashboard/dashboard');
+})->name('dashboard');
+
+
+Route::prefix('admin')->group(function () {
+    Route::resource('advs', AdvController::class);
+    Route::resource('categories', CategoryController::class);
+    Route::resource('comments', CommentController::class);
+    Route::resource('posts', PostController::class);
+    Route::resource('post-tags', PostTagController::class);
+    Route::resource('post-types', PostTypeController::class);
+    Route::resource('tags', TagController::class);
+    Route::resource('users', UserController::class);
+    Route::get('/dashboard', function () {
+        return view('dashboard/dashboard');
+    })->name('dashboard');
 });
